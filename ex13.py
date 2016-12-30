e765d70e2b3d95d0cdbd254ebe046dd6a9eeee84
yuu <yuu-@users.noreply.github.com>
2016-12-30 08:58:44 +0800

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# argv: argument variable
+from sys import argv
+
+# unpack
+scipt, first, second, third = argv
+
+print "The script is called," scipt
+print "Your first varialbe is ",first
+print "Your secong variable is:", second
+print "your shird variable is:", third
